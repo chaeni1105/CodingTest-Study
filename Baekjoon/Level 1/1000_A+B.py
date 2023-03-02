07c5dfbb46af9682c609658110f46190c222782c
Chaeeun Lee <cyleecodms@naver.com>
2023-03-02 21:29:43 +0900

    2023.03.02(2)

--- Baekjoon/Level 1/1000_A+B.py
+++ Baekjoon/Level 1/1000_A+B.py
@@ -5,5 +5,5 @@
 ### [문제 풀이]
 # 1단계 : 두 정수 A와 B를 입력받은 다음, print()함수를 이용해서 출력한다.
 
-A, B = list(map(int, input().split()))
+A, B = map(int, input().split())
 print(A + B)
---
+++ Baekjoon/Level 1/10869_사칙연산.py
@@ -0,0 +1,13 @@
+### [문제 분석]
+# 목적 : A+B, A-B, A*B, A/B(몫), A%B(나머지) 출력
+# -> A+B, A-B, A*B, A/B(몫), A%B(나머지) 출력시키기
+
+### [문제 풀이]
+# 1단계 : 두 정수 A와 B를 입력받은 다음, print()함수를 반복적으로 이용해서 출력한다.
+
+A, B = list(map(int, input().split()))
+print(A + B)
+print(A - B)
+print(A * B)
+print(A / B)
+print(A % B)

9de6b40a6a4a1cdc57e7a65eb232b264ca502918
Chaeeun Lee <cyleecodms@naver.com>
2023-03-02 21:37:33 +0900

    2023.03.02(3)

--- Baekjoon/Level 1/10869_사칙연산.py
+++ Baekjoon/Level 1/10869_사칙연산.py
@@ -3,11 +3,11 @@
 # -> A+B, A-B, A*B, A/B(몫), A%B(나머지) 출력시키기
 
 ### [문제 풀이]
-# 1단계 : 두 정수 A와 B를 입력받은 다음, print()함수를 반복적으로 이용해서 출력한다.
+# 1단계 : 두 정7 3수 A와 B를 입력받은 다음, print()함수를 반복적으로 이용해서 출력한다.
 
-A, B = list(map(int, input().split()))
+A, B = map(int, input().split())
 print(A + B)
 print(A - B)
 print(A * B)
-print(A / B)
+print(A // B)
 print(A % B)

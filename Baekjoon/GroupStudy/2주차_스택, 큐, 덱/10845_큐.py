83d6f36494c2cf7c92dfaa7ee927b09a87cbbd8f
Chaeeun Lee <cyleecodms@naver.com>
2023-03-10 16:08:48 +0900

    2023.03.10

--- Baekjoon/GroupStudy/2주차_스택, 큐, 덱/10845_큐.py
+++ Baekjoon/GroupStudy/2주차_스택, 큐, 덱/10845_큐.py
@@ -12,10 +12,25 @@ import sys
 input = sys.stdin.readline
 
 # 1단계
+from collections import deque #데큐 자료구조
+
 N = int(input())
-queue = []
+queue = deque()
 
 # 2단계
-
+for _ in range(N):
+    cmd = input().split()
+    
+    if(cmd[0] == 'push'):
+        queue.append(cmd[1])
+    
+    elif(cmd[0] == 'pop'):
+        print(queue[1])
+        queue.popleft()
 
 # 3단계    
+
+# 주의할 점
+# 1. 파이썬으로 큐를 구현할 때는 collections 모듈에서 제공하는 deque 자료구조를 활용하자.
+# 데이터를 넣고 빼는 속도가 리스트 자료형에 비해 효율적이며 queue 라이브러리보다 간단하다.
+# 대부분의 코테에서는 collections 모듈과 같은 기본 라이브러리 사용을 허용한다.

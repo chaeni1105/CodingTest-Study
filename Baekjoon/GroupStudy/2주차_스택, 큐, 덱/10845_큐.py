8b4d6d50579cb6f63b8b48d4afc49274dafc68a4
Chaeeun Lee <cyleecodms@naver.com>
2023-03-10 22:33:13 +0900

    2023.03.10(2)

--- Baekjoon/GroupStudy/2주차_스택, 큐, 덱/10845_큐.py
+++ Baekjoon/GroupStudy/2주차_스택, 큐, 덱/10845_큐.py
@@ -19,18 +19,43 @@ queue = deque()
 
 # 2단계
 for _ in range(N):
-    cmd = input().split()
+    cmd = list(input().split()) #리스트로 받아줘야함!
     
     if(cmd[0] == 'push'):
-        queue.append(cmd[1])
+        queue.append(cmd[1]) #정수를 큐에 넣는다.
     
     elif(cmd[0] == 'pop'):
-        print(queue[1])
-        queue.popleft()
-
-# 3단계    
-
+        if(len(queue) == 0):
+            print(-1)
+        else:
+            print(queue[0])
+            queue.popleft() #가장 앞에있는 정수를 큐에서 삭제한다.
+        
+    elif(cmd[0] == 'size'):
+        print(len(queue)) #큐에 들어있는 정수의 개수를 출력한다.
+        
+    elif(cmd[0] == 'empty'):
+        if(len(queue) == 0):
+            print(1)
+        else:
+            print(0)
+        
+    elif(cmd[0] == 'front'):
+        if(len(queue) == 0):
+            print(-1)
+        else:
+            print(queue[0]) #큐의 가장 앞에 있는 정수를 출력한다.
+        
+    elif(cmd[0] == 'back'):
+        if(len(queue) == 0):
+            print(-1)
+        else:
+            print(queue[-1]) #큐의 가장 뒤에 있는 정수를 출력한다.
+            
 # 주의할 점
 # 1. 파이썬으로 큐를 구현할 때는 collections 모듈에서 제공하는 deque 자료구조를 활용하자.
 # 데이터를 넣고 빼는 속도가 리스트 자료형에 비해 효율적이며 queue 라이브러리보다 간단하다.
 # 대부분의 코테에서는 collections 모듈과 같은 기본 라이브러리 사용을 허용한다.
+
+# 2. 조건을 항상 잘 확인하자.
+# elif(cmd[0] == 'pop'):을 할때, 만약 큐가 비어있으면 -1을 출력해야함을 간과했다 ㅠㅠ

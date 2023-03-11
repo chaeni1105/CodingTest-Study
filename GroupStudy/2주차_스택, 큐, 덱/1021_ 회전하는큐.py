b999243234996aa4fe09cdc0054addc601c38a0b
Chaeeun Lee <cyleecodms@naver.com>
2023-03-11 23:50:49 +0900

    2023.03.11(3)

--- GroupStudy/2주차_스택, 큐, 덱/1021_ 회전하는큐.py
+++ GroupStudy/2주차_스택, 큐, 덱/1021_ 회전하는큐.py
@@ -7,8 +7,8 @@
 # 1단계 : 1. 큐의 크기 N, 뽑아내려고 하는 수의 개수 M을 입력받는다. queue 리스트를 만든다.
 #         2. 지민이가 뽑아내려고 하는 수의 위치를 순서대로 입력받는다.
 #         3. 큐 리스트 안에 수를 넣어놓는다.
-# 2단계 : 
-# 3단계 :
+# 2단계 : 만약 뽑을 번호가 절반을 기점으로 더 왼쪽에 있으면 2번연산을 하고,가운데보다 더 오른쪽에 있으면 3번연산을 행한다.
+#         그리고, 2,3번 연산을 통해 뽑아야할 수가 맨 앞으로 오면 제거하고 while문을 빠져나온다.
 
 import sys
 input = sys.stdin.readline
@@ -30,41 +30,37 @@ cnt3 = 0
 
 # for i in range(0, M):
 #     while True:
+#         if(queue.index(num[i]) < len(queue)/2): #2의 index번호는 현재 1이다. 따라서 5보다 작기때문에 왼쪽수를 오른쪽으로 이동한다.
+#             while(queue[0] != num[i]):
+#                 queue.append(queue.popleft()) #가장 앞(왼쪽) 수를 삭제한다.
+#                 cnt2 += 1
+
+#         else: #9의 index번호는 7이다. 따라서 4.5보다 크기 때문에 오른쪽수를 왼쪽으로 이동한다.
+#             while(queue[0] != num[i]):
+#                 queue.appendleft(queue.pop()) #가장 뒤(오른쪽) 수를 삭제한다.
+#                 cnt3 += 1
+                
 #         if(queue[0] == num[i]):
 #             queue.popleft()
 #             break
-#         else:
-#             if(queue.index(num[i]) < len(queue)/2): #2의 index번호는 현재 1이다. 따라서 5보다 작기때문에 왼쪽수를 오른쪽으로 이동한다.
-#                 while(queue[0] != num[i]):
-#                     queue.append(queue[0])
-#                     queue.popleft() #가장 앞(왼쪽) 수를 삭제한다.
-#                     cnt2 += 1
-#                     break
-#             else: #9의 index번호는 7이다. 따라서 4.5보다 크기 때문에 오른쪽수를 왼쪽으로 이동한다.
-#                 while(queue[0] != num[i]):
-#                     queue.appendleft(queue[0])
-#                     queue.pop() #가장 뒤(오른쪽) 수를 삭제한다.
-#                     cnt3 += 1
-#                     break
+                
 # print(cnt2 + cnt3)
 
 for i in range(0, M):
-    while True:
-        if(queue.index(num[i]) < len(queue)/2): #2의 index번호는 현재 1이다. 따라서 5보다 작기때문에 왼쪽수를 오른쪽으로 이동한다.
-            while(queue[0] != num[i]):
-                queue.append(queue[0])
-                queue.popleft() #가장 앞(왼쪽) 수를 삭제한다.
-                cnt2 += 1
+    if(queue.index(num[i]) < len(queue)/2): #2의 index번호는 현재 1이다. 따라서 5보다 작기때문에 왼쪽수를 오른쪽으로 이동한다.
+        while(queue[0] != num[i]):
+            queue.append(queue.popleft()) #가장 앞(왼쪽) 수를 삭제한다.
+            cnt2 += 1
 
-        elif(queue.index(num[i]) > len(queue)/2): #9의 index번호는 7이다. 따라서 4.5보다 크기 때문에 오른쪽수를 왼쪽으로 이동한다.
-            while(queue[0] != num[i]):
-                queue.appendleft(queue[0])
-                queue.pop() #가장 뒤(오른쪽) 수를 삭제한다.
-                cnt3 += 1
-
-        elif(queue[0] == num[i]):
-            queue.popleft()
-            break
+    else: #9의 index번호는 7이다. 따라서 4.5보다 크기 때문에 오른쪽수를 왼쪽으로 이동한다.
+        while(queue[0] != num[i]):
+            queue.appendleft(queue.pop()) #가장 뒤(오른쪽) 수를 삭제한다.
+            cnt3 += 1
+                
+    if(queue[0] == num[i]):
+        queue.popleft()
+            
+                
 print(cnt2 + cnt3)
             
             
@@ -73,10 +69,14 @@ print(cnt2 + cnt3)
 #오른쪽3칸이동(9,10,1,3,4,5,6,7,8) -> 9를 뽑아내기.
 #오른쪽4칸이동(5,6,7,8,9,10,1,3,4) -> 5를 뽑아내기.
 #총합 8!!!
-
-# 3단계
-
     
 # 주의할 점
 # 1. 큐는 선입선출 방식이지만 덱은 양방향에서 삽입과 삭제 가능하기에 시간 복잡도가 O(1)이므로 deque를 이용하여 양방향 순환 큐의 문제를 해결하면 된다.
-# 2. 
+
+# 풀리지 않는 의문. 물어보기 ㄱ
+# queue.append(queue.popleft()) 가장 앞(왼쪽) 수를 삭제할 때, 이렇게 하지 않고,
+
+# queue.append(queue[0])
+# queue.popleft() 
+# 이렇게 두 줄로 하면 시간초과가 뜬다. 그 이유가 무엇일까?
+# 사실 while문이 있어야하는 이유를 모르겠다. -> 없어도 된다! 어차피 2,3번 연산안에 while문이 존재하여 맨 앞으로 수가 갈때까지 반복하므로!!

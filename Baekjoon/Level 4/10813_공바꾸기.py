9e14fdd13132eb59bfa88155c905a1e243b9c961
Chaeeun Lee <cyleecodms@naver.com>
2023-03-15 22:42:14 +0900

    2023.03.15(2)

--- Baekjoon/Level 4/10813_공바꾸기.py
+++ Baekjoon/Level 4/10813_공바꾸기.py
@@ -3,9 +3,9 @@
 # -> 공을 교환하고, 1번 바구니부터 N번 바구니에 들어있는 공의 번호를 공백으로 구분해 출력한다.
 
 ### [문제 풀이]
-# 1단계 : 
-# 2단계 : 
-# 3단계 :
+# 1단계 : 총 바구니의 개수 N, 총 공을 넣는 횟수 M를 입력받고, [1, 2, 3, 4, 5] 배열 생성한다.
+# 2단계 : i번과 j번 바구니에 들어있는 공을 서로 바꾼다. 이는 M번 반복하는 반복문 안에 쓴다.
+# 3단계 : 1번 바구니부터 N번 바구니에 들어있는 공의 번호를 공백으로 구분해 출력한다.(출력 모양 조심!)
 
 
 import sys
@@ -18,17 +18,15 @@ answer = []
 for i in range(1, N+1): # [1, 2, 3, 4, 5] 배열 생성
     answer.append(i)
 
-
-for _ in range(M): #공을 M번 넣는만큼 반복
-    i, j, k = map(int, input().split()) #i번과 j번 바구니에 들어있는 공을 서로 바꾼다.
+for _ in range(M): #공을 M번 바꾸는만큼 반복
+    i, j = map(int, input().split())
 
     # 2단계
-    for num in range (i-1, j): #1번바구니~2번바구니까지 0 -> 3으로 공을 바꿔치기함.
-        answer[num] = k
+    #1번바구니와 2번바구니의 공을 서로 바꿔치기함.
+    temp = answer[i-1]
+    answer[i-1] = answer[j-1]
+    answer[j-1] = temp
 
 # 3단계
 for i in range(N):
     print(answer[i], "", end='')
-
-    
-# 주의할 점

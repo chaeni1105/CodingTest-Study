e011e9c4e9affda46c2d88a97b0ea297cc86e727
Chaeeun Lee <cyleecodms@naver.com>
2023-04-06 17:18:46 +0900

    2023.04.06

--- Baekjoon/Level 1/2588_곱셈.py
+++ Baekjoon/Level 1/2588_곱셈.py
@@ -1,5 +1,5 @@
 ### [문제 분석]
-# 목적 : (세 자리 수) × (세 자리 수) 곱하기
+# 목적 : (세 자리 수) × (세 자리 수) 곱셈 과정을 코드로 나타내 보는 문제
 # -> 세자리 수를 곱할 때 나타나는 값 3개를 더해서 최종 값까지 총 4개를 구하기.
 
 ### [문제 풀이]
@@ -16,6 +16,7 @@ B = int(input())
 num1 = A * (B%10)
 num2 = A * ((B//10)%10)
 num3 = A * (B//100)
+
 # 3단계
 num4 = num1 + 10*num2 + 100*num3
 
@@ -24,3 +25,22 @@ print(num1)
 print(num2)
 print(num3)
 print(num4)
+
+
+# # 1단계
+# A = int(input())
+# B = input()
+
+# # 2단계
+# num1 = A * int(B[2])
+# num2 = A * int(B[1])
+# num3 = A * int(B[0])
+
+# # 3단계
+# num4 = num1 + 10*num2 + 100*num3
+
+# # 4단계
+# print(num1)
+# print(num2)
+# print(num3)
+# print(num4)
